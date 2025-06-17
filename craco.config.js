bcd06fc84d7caaa1348e22be3c6f48f676b3e8ad
Your Name <your.email@example.com>
2025-06-17 22:50:29 +0530

    UI update

--- craco.config.js
+++ craco.config.js
@@ -4,7 +4,18 @@ module.exports = {
       resolve: {
         fallback: {
           "ajv": require.resolve("ajv"),
+          "path": require.resolve("path-browserify"),
+          "crypto": require.resolve("crypto-browserify"),
+          "stream": require.resolve("stream-browserify"),
+          "buffer": require.resolve("buffer/"),
+          "util": require.resolve("util/"),
         }
+      },
+      optimization: {
+        splitChunks: {
+          chunks: 'all',
+        },
+        runtimeChunk: true,
       }
     }
   }
